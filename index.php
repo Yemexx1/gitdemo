b1ff114f892e79660594eb76d24bb419c44c46d0
Matthew Mayaki <dynamicmax5000@gmail.com>
2015-01-20 12:13:18 +0100

    added mayakiz index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<?php
+			echo "<h1>Welcome to the world of blah</h1>";
+		?>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam id aut, voluptate quos nam consectetur vero, quibusdam, ipsam aliquam iste enim non. Nam iusto natus voluptate, ducimus accusamus reprehenderit aut?
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas quasi corporis, ad harum, dignissimos reiciendis asperiores! A reprehenderit accusamus asperiores amet nemo. Beatae recusandae distinctio sit deleniti perferendis nihil, consectetur.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem mollitia odit explicabo cumque quod asperiores animi autem libero, aperiam voluptatum at non, assumenda et, natus ex facere minus, dolorem dicta.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur magnam, veritatis doloremque ex maxime, possimus ratione incidunt vel ipsum soluta, labore. Tenetur et voluptatem obcaecati officiis incidunt odit repudiandae fugit.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet aperiam veritatis totam, sint voluptatibus libero pariatur iste facilis obcaecati recusandae veniam, voluptatum inventore ex minima perspiciatis. Asperiores quae, voluptatum labore.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci perspiciatis expedita, dicta ea neque distinctio delectus ab animi quod! Asperiores tempore dolorem, velit reprehenderit voluptate, ipsum? Ratione perspiciatis quaerat, ex?
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit vitae harum rerum consectetur aut eveniet distinctio incidunt mollitia accusantium culpa voluptatem voluptatum possimus, delectus quam iure non cumque cupiditate. Temporibus.
+	</body>
+</html>
